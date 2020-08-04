bc37c4b22fa2c406f2941f3c5a67660b460684e3
Aaron <toscleveland@gmail.com>
2020-08-03 21:32:06 -0500

    Started Weather Model

---
+++ Windie/Model/Weather.swift
@@ -0,0 +1,76 @@
+//
+//  Weather.swift
+//  Windie
+//
+//  Created by Aaron Cleveland on 8/3/20.
+//  Copyright © 2020 Aaron Cleveland. All rights reserved.
+//
+
+// https://openweathermap.org/current
+// https://www.hackingwithswift.com/books/ios-swiftui/sending-and-receiving-codable-data-with-urlsession-and-swiftui
+
+import Foundation
+
+// MARK: - Weather
+struct Weather: Codable {
+    let coord: Coord
+    let weather: [WeatherElement]
+    let base: String
+    let main: Main
+    let wind: Wind
+    let clouds: Clouds
+    let dt: Int
+    let sys: Sys
+    let timezone, id: Int
+    let name: String
+    let cod: Int
+}
+
+// MARK: - Clouds
+struct Clouds: Codable {
+    let all: Int
+}
+
+// MARK: - Coord
+struct Coord: Codable {
+    let lon, lat: Int
+}
+
+// MARK: - Main
+struct Main: Codable {
+    let temp, feelsLike, tempMin, tempMax: Double
+    let pressure, humidity: Int
+
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+        case pressure, humidity
+    }
+}
+
+// MARK: - Sys
+struct Sys: Codable {
+    let type, id: Int
+    let message: Double
+    let country: String
+    let sunrise, sunset: Int
+}
+
+// MARK: - WeatherElement
+struct WeatherElement: Codable {
+    let id: Int
+    let main, weatherDescription, icon: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, main
+        case weatherDescription = "description"
+        case icon
+    }
+}
+
+// MARK: - Wind
+struct Wind: Codable {
+    let speed, deg: Double
+}
